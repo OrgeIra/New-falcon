ceed3413ca06ab340387c79d076a1323fbbdcfda
OrgeIra <dodomatovabdusami@gmail.com>
2025-02-20 11:32:45 +0500

    Sebd email

--- core/settings.py
+++ core/settings.py
@@ -140,8 +140,8 @@ from decouple import config
 
 EMAIL_BACKEND = config("EMAIL_BACKEND")
 EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)  # Convert to integer
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)  # Convert to boolean
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)  
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
